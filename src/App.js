996d6960591f6e387b0d99e2600c0175f2356529
jkory7 <jony.choi7@gmail.com>
2022-01-27 18:47:03 -0600

    Added new components: Home, Navbar. Those components added to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Navbar';
+import Home from './Home';
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>App component</h1>
+        <Home />
       </div>
     </div>
   );
